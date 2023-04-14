3f2e3d1bef2491eee37fd42972f031d7ef2ec96f
James England <jimmy.england@kellogg.northwestern.edu>
2023-04-14 00:48:21 +0000

    q7 complete

--- practice-7.sql
+++ practice-7.sql
@@ -16,4 +16,9 @@
 -- | Entire townhouse                    | 3588     | 2021-10-17                 |
 -- | Entire villa                        | 75       | 2021-10-12                 |
 
-
+Select property_type, count(reviews.id), date_reviewed
+FROM listings
+INNER JOIN reviews
+ON listings.id = reviews.listing_id 
+GROUP BY property_type
+;
